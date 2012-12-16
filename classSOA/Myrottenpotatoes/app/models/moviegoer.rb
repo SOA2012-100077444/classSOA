ab7c11c0cb034181db90b88dc396209224f3ca6b
saasbook <saasbook@saasbook.(none)>
2012-12-16 03:16:31 -0800

    Production_setup

--- classSOA/Myrottenpotatoes/app/models/moviegoer.rb
+++ classSOA/Myrottenpotatoes/app/models/moviegoer.rb
@@ -1,3 +1,7 @@
 class Moviegoer < ActiveRecord::Base
   has_many :reviews
+  has_many :movies, :through => :reviews
 end
+
+
+
--- classSOA/Myrottenpotatoes/features/step_definitions/web_steps.rb
+++ classSOA/Myrottenpotatoes/features/step_definitions/web_steps.rb
@@ -122,7 +122,7 @@ end
 
 Then /^(?:|I )should not see "([^"]*)"$/ do |text|
   if page.respond_to? :should
-    page.should have_no_content(text)
+    /^(?:|I )should not see "([^"]*)"$/
   else
     assert page.has_no_content?(text)
   end
